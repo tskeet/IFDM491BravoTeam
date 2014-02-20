6301a2cd6037ae7429dc6ab9e4d115ef1b8d96e9
Tyrell Skeet <tskeet.47@gmail.com>
2014-02-20 13:52:15 -0700

    I added a test1 scence
    This scene is just a place where I can test changing out the universes
    and working with the AI

---
+++ TeamBravoProject/Assets/Scripts/EnemyAI.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var player : Transform;
+var enemy : Transform;
+var speed : float;
+var maxSpeed : float;
+
+function Start () {
+
+}
+
+function Update () {
+
+	var newVector : Vector3;
+	newVector = enemy.position - player.position;
+	newVector.Normalize();
+	newVector = newVector * speed;
+	enemy.position = newVector;
+}
