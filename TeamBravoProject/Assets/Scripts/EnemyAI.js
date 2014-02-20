99fe5312d53ff976779dc0e67e8bca21b0a2a50d
tskeet <tskeet.47@gmail.com>
2014-02-20 15:25:26 -0700

    enemies added to test1
    very stupid enemies added to test1

--- TeamBravoProject/Assets/Scripts/EnemyAI.js
+++ TeamBravoProject/Assets/Scripts/EnemyAI.js
@@ -1,6 +1,6 @@
 ﻿#pragma strict
 
-var player : Transform;
+/**var player : Transform;
 var enemy : Transform;
 var speed : float;
 var maxSpeed : float;
@@ -16,4 +16,38 @@ function Update () {
 	newVector.Normalize();
 	newVector = newVector * speed;
 	enemy.position = newVector;
+}**/
+
+/*var enemy : Transform;
+var speed: float;
+     
+function Update() {
+    if (  Vector3.Distance( enemy.position, transform.position) < 100  ) {
+        print("player is close");
+         
+        transform.position = (enemy.position - transform.position ).normalized * speed * Time.deltaTime;
+    } else {
+        print("not close yet");
+        print(Vector3.Distance( enemy.position, transform.position ));
+    }
+}*/
+
+var target : Transform;
+var moveSpeed : float = 3f;
+var rotationSpeed : float = 3f;
+
+var myTransform : Transform;
+
+function Awake() {
+	myTransform = transform;
+}
+
+function Start() {
+	target = GameObject.FindWithTag("Player").transform;
+}
+
+function Update() {
+	myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed*Time.deltaTime);
+	
+	myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
 }
