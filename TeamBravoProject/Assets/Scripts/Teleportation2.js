a922c28db07b85571a38aef305d5f8cdba8e922b
Tyrell Skeet <tskeet.47@gmail.com>
2014-02-23 22:27:31 -0700

    New Test Level
    I added in a new test level, and I made the project compatible with an
    xbox 360 controller, though I doesn't work with a keyboard and mouse
    anymore.  I have to figure out do both, if it's possible.

---
+++ TeamBravoProject/Assets/Scripts/Teleportation2.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+var currentUniverse : int;
+var player : Transform;
+var universe1Origin : Vector3;
+var universe2Origin : Vector3;
+var universe3Origin : Vector3;
+var universe4Origin : Vector3;
+
+
+function Start () {
+
+}
+
+function Update () {
+
+	if(Input.anyKeyDown) {
+		Debug.Log(Input.inputString);
+	
+	}
+
+	if(Input.GetKeyDown(KeyCode.UpArrow)) {
+		Debug.Log("Up arrow down.");
+	}
+	if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+		Debug.Log("Left arrow down.");
+	}
+	if(Input.GetKeyDown(KeyCode.DownArrow)) {
+		Debug.Log("Down arrow down.");
+	}
+	if(Input.GetKeyDown(KeyCode.RightArrow)) {
+		Debug.Log("Right arrow down.");
+	}
+}
