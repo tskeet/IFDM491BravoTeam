76cbea4ba7983270be1ed1d04822f32387bd05c9
tskeet <tskeet.47@gmail.com>
2014-03-03 21:37:54 -0700

    black hole projectiles and gun
    added black hole projectiles and fixed the problem with the gun going
    through the walls.

--- TeamBravoProject/Assets/Scripts/GunScript.js
+++ TeamBravoProject/Assets/Scripts/GunScript.js
@@ -4,6 +4,7 @@
  * This script is for the gun.
  **/
 private var currentTrigger : boolean;
+public var projectile : Rigidbody;
 
 function Start () {
 	currentTrigger = false;
@@ -15,6 +16,10 @@ function Update () {
 	
 	if((!currentTrigger) && rightTrigger) {
 		Debug.Log("left Trigger is down!  Fire!");
+		
+		var clone : Rigidbody;
+		clone = Instantiate(projectile, transform.position, transform.rotation);
+		clone.velocity = transform.TransformDirection(Vector3.forward * 50);
 	}
 	
 	currentTrigger = rightTrigger;
