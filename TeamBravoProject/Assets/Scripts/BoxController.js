6af34eb5736c5b5d3178915d79ca801817346027
Tyrell Skeet <tskeet.47@gmail.com>
2014-03-09 21:48:36 -0600

    made the boxes work with changing the universes

---
+++ TeamBravoProject/Assets/Scripts/BoxController.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+public var nearBox : boolean;
+public var inBox : boolean;
+public var player : Teleportation2;
+public var weapon : GunScript;
+public var currentBox : BoxScript;
+
+function Start () {
+	nearBox = false;
+	inBox = false;
+}
+
+function Update () {
+
+	if(Input.GetButtonDown("X_Button") && nearBox) {
+		Debug.Log("x button was pressed");
+		if(inBox) {
+			// take weapon out of box and change weapon's current Universe
+			weapon.currentUniverse = player.currentUniverse;
+			inBox = false;
+		} else {
+			// put weapon into box and change weapon's current universe to zero
+			//You can only put a weapon into the box when the player and gun exist in the same universe
+			if(weapon.currentUniverse == player.currentUniverse) {
+				weapon.currentUniverse = 0;
+				inBox = true;
+			}
+		}
+	}
+}
---
+++ TeamBravoProject/Assets/Scripts/BoxScript.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+private var anotification : GameObject;
+public var boxController : BoxController;
+function Start () {
+}
+
+function Update () {
+
+/**	if(Input.GetButtonUp("X_Button")) {
+		//Debug.Log("x button pushed");
+		Debug.Log("inBox = " + inBox);
+		if(nearBox) {
+			if(inBox) {
+				OutOfBox();
+			} else{ 
+				InToBox();
+			}
+		}
+	}**/
+}
+
+function OnTriggerEnter(other : Collider) {
+	//Debug.Log("collided with box");
+	if(other.CompareTag("Player")) {
+		anotification = Instantiate(new GameObject(), new Vector3(0.5, 0.5, 0.5), Quaternion.identity);
+		anotification.AddComponent(GUIText);
+		if(boxController.inBox) {
+			anotification.guiText.text = "Press X to take weapon out of box";
+		} else {
+			anotification.guiText.text = "Press X to put weapon into box";
+		}
+		boxController.nearBox = true;
+		boxController.currentBox = this;
+		//Debug.Log("Press A to put weapon into box.");
+	}
+}
+
+function OnTriggerExit(other : Collider) {
+	if(other.CompareTag("Player")) {
+		Destroy(anotification);
+		boxController.nearBox = false;
+	}
+}
