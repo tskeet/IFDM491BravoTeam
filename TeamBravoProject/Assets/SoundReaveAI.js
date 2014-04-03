6313562b10ed8b05ffb77cf811a3ca2a3012a6aa
tskeet <tskeet.47@gmail.com>
2014-04-02 19:46:24 -0600

    Hearing Reave

---
+++ TeamBravoProject/Assets/SoundReaveAI.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+public var target : Transform;
+
+private var timer : int;
+private var currentState : SoundReaveState;
+
+enum SoundReaveState{EchoLocate, EchoFound, Attack, Wander};
+
+function Start () {
+	timer = 30;
+	//Debug.Log("DeltaTime is " + Time.deltaTime);
+	currentState = SoundReaveState.Wander;
+}
+
+function Update () {
+	
+	if(currentState == SoundReaveState.Attack) {
+		this.AttackFunction();
+	} else if(currentState == SoundReaveState.EchoLocate) {
+		this.EchoLocationFunction();
+	} else if(currentState == SoundReaveState.EchoFound) {
+		this.EchoFoundFunction();
+	} else {
+		this.WanderFunction();
+	}
+	//Debug.Log("DeltaTime is " + Time.deltaTime);
+}
+
+function EchoLocationFunction() {
+
+}
+
+function EchoFoundFunction() {
+
+}
+
+function AttackFunction() {
+
+}
+
+function WanderFunction() {
+
+}
