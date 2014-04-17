b8769dfca72ab35c0df52a6634c05b5ac77f54e4
tskeet <tskeet.47@gmail.com>
2014-04-17 16:07:42 -0600

    continue work on the Health Bar

--- TeamBravoProject/Assets/Scripts/HealthScript.js
+++ TeamBravoProject/Assets/Scripts/HealthScript.js
@@ -1,13 +1,16 @@
 ﻿#pragma strict
 
 public var health : float = 100.0;
-public var position : Vector2;
+public var position : Rect;
+public var healthbar : Texture2D;
+public var healthgreen : Texture2D;
 
 function Start () {
 
 }
 
 function Update () {
+	//position = Rect((Screen.width - 
 	if (health <= 0.0) {
 		//Player is dead
 		Debug.Log("player is dead");	
@@ -18,4 +21,8 @@ function Update () {
 
 function DamagePlayer(damage : float) {
 	health = health - damage;
+}
+
+function OnGUI() {
+	//GUI.DrawTexture
 }
