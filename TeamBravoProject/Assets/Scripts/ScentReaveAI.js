966e08c06e803a9e4dab19d4a2811580a4100b60
tskeet <tskeet.47@gmail.com>
2014-04-24 14:09:02 -0600

    updated the scent reave

--- TeamBravoProject/Assets/Scripts/ScentReaveAI.js
+++ TeamBravoProject/Assets/Scripts/ScentReaveAI.js
@@ -2,19 +2,32 @@
 
 public var target : Transform;
 
+private var agent : NavMeshAgent;
+
 private var timer : int;
-private var currentState : ScentReaveState;;
+public var currentState : ScentReaveState;
 private var previousPlayerLocation : Vector3;
 
 
-public var scentNodeCopy : GameObject;
-public var scentTrail : Array;
-public var maxScentTrailLength : int;
+public var scentNodeCopy : GameObject; //a reference to the scent nodes that the GameObject.Instantiate method will use to create new scent nodes
+//public var scentTrail : Array;
+public var scentTrail : GameObject[]; //a collection of scentNodes
+public var maxScentTrailLength : int; //maximum length of scent trail
+public var scentIndex : int; //keeps track of current index in scentTrail
+
+public var nextIndex : int;
 
 enum ScentReaveState {Follow, Wander, Attack};
+
 function Start () {
-	scentTrail = new Array();
+	//scentTrail = new Array();
+	agent = GetComponent.<NavMeshAgent>();
+	timer = 70;
+	currentState = ScentReaveState.Wander;
 	previousPlayerLocation = target.position;
+	scentTrail = new GameObject[maxScentTrailLength];
+	
+	nextIndex = -1;
 }
 
 function Update () {
@@ -42,10 +55,12 @@ function FollowFunction() {
 }
 
 function WanderFunction() {
-
+	var randomVector = Random.insideUnitCircle * 5;
+	agent.speed = 5.0;
+	agent.SetDestination((new Vector3(randomVector.x, 0.0, randomVector.y)) + agent.transform.position);
 }
 
-function AddToScentTrail() {
+/**function AddToScentTrail() {
 	if(Vector3.Distance(previousPlayerLocation, target.position) > 1.0) {
 		var midpoint = Vector3.Lerp(previousPlayerLocation, target.position, 0.5);
 		//var angle = 
@@ -55,5 +70,38 @@ function AddToScentTrail() {
 		}
 		scentTrail.Add(newScentNode);
 		previousPlayerLocation = target.position;
+	}
+}*/
+
+function AddToScentTrail() {
+	if(Vector3.Distance(previousPlayerLocation, target.position) > 1.0) {
+		var newScentNode : GameObject = GameObject.Instantiate(scentNodeCopy, target.position, target.rotation);  
+		if(scentIndex >= maxScentTrailLength) {
+			scentIndex = 0;
+		}
+		
+		if(scentTrail[scentIndex] != null) {
+			GameObject.Destroy(scentTrail[scentIndex]);
+		}
+		
+		scentTrail[scentIndex] = newScentNode;
+		
+		var tempScript : ScentNodeScript = newScentNode.GetComponent("ScentNodeScript");
+		tempScript.SetIndex(scentIndex);
+		
+		scentIndex++;
+		previousPlayerLocation = target.position;
+	}
+}
+
+function OnTriggerEnter(other : Collider) {
+	if(other.CompareTag("ScentNode") && (nextIndex < 0)) {
+		agent.Stop(true);
+		currentState = ScentReaveState.Follow;
+		var tempScript : ScentNodeScript = other.gameObject.GetComponent("ScentNodeScript");
+		Debug.Log("triggered ScentNode with index " + tempScript.GetIndex());
+	} else if(other.gameObject.tag == "Player") {
+		Debug.Log("damaging the player.");
+		//ayerHealth.DamagePlayer(10.0);
 	}
 }
