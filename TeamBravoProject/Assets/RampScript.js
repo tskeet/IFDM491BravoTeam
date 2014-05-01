0226c997659c657d4da9e8e26d3c8bffdd58404b
tskeet <tskeet.47@gmail.com>
2014-04-30 19:55:27 -0600

    ramp fixed

---
+++ TeamBravoProject/Assets/RampScript.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var savedMaxForwardSpeed : float;
+var savedAcceleration : float;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(other : Collider) {
+	if(other.CompareTag("Player")) {
+		Debug.Log("entered ramp");
+		var characterMotor : CharacterMotor = other.gameObject.GetComponent("CharacterMotor");
+		savedMaxForwardSpeed = characterMotor.movement.maxForwardSpeed;
+		savedAcceleration = characterMotor.movement.maxGroundAcceleration;
+		characterMotor.movement.maxForwardSpeed = 1000.0;
+		characterMotor.movement.maxGroundAcceleration = 1000.0;
+	}
+}
+
+function OnTriggerExit(other : Collider) {
+	if(other.CompareTag("Player")) {
+		Debug.Log("exited ramp");
+		var characterMotor : CharacterMotor = other.gameObject.GetComponent("CharacterMotor");
+		characterMotor.movement.maxForwardSpeed = savedMaxForwardSpeed;
+		characterMotor.movement.maxGroundAcceleration = savedAcceleration;
+	}
+}
